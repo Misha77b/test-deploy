311dd6392ebf69d57950842bacbe6a0b807b1edb
Misha <misha77b@gmail.com>
2023-03-10 20:53:30 +0200

    build

---
+++ config/keys_dev.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI:
+    'mongodb+srv://Admin_fe4:fe4group2@cluster0.ywjxnvu.mongodb.net/?retryWrites=true&w=majority',
+  secretOrKey: 'random very very secret string',
+};
